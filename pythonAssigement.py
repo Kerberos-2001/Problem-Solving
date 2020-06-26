b428161466d090bb586a52c74a02838ec7783d58
Prayag <prayagpiya12@gmail.com>
2020-06-26 19:28:33 +0545

    [This is third commit]

--- pythonAssigement.py
+++ pythonAssigement.py
@@ -75,8 +75,10 @@ print(swapUp(quest3, quest4))
 def addString(string):
     if len(string) >= 3 and string[-3:] != 'ing':
         string = string + 'ing'
+        return string
     elif len(string) >= 3 and string[-3:] == 'ing':
         string = string + 'ly'
+        return string
     else:
         return string
 
@@ -89,10 +91,9 @@ print(addString(quest5))
 
 
 def substitute(string):
-    indexNot = string('not')
-    indexPoor = string('poor')
-
-    if indexPoor > indexNot:
+    indexNot = string.find('not')
+    indexPoor = string.find('poor')
+    if indexPoor > indexNot and indexNot > 0 and indexPoor > 0:
         string = string.replace(string[indexNot:indexPoor+4], 'good')
         return string
     else:
@@ -100,7 +101,69 @@ def substitute(string):
 
 
 quest6 = input('Change everthing followed by not to good >> ')
+print()
 print(substitute(quest6))
 
 
-# 7)
+# 7)  Write a Python function that takes a list of words and returns the length of the longest one.
+
+def countLength(array):
+    array = array.split(',')
+    count = 0
+    if isinstance(array, list):
+        for i in range(1, len(array)):
+            if len(array[count]) > len(array[i]):
+                count = count
+            else:
+                count = i
+        return array[count]
+
+
+quest7 = input(
+    'Calculate the longest length of word in list, seperate word by comma >> ')
+print()
+print(countLength(quest7))
+
+
+# 8) Write a Python program to remove the nth index character from a nonempty string.
+
+def removeChar(string, index):
+    if len(string) > 0:
+        return string[:index] + string[index+1:]
+
+
+quest8 = input(
+    'Remove the nth index of the character from nonempty string >> ')
+index = int(input('Enter the index >> '))
+print()
+print(removeChar(quest8, index))
+
+# 9) Write a Python program to change a given string to a new string where the first and last chars have been exchanged.
+
+
+def exchangeChar(string):
+    return string[-1:]+string[1:-1]+string[:1]
+
+
+quest9 = input('Exchange the strings first and last chars >> ')
+print()
+print(exchangeChar(quest9))
+
+# 10) Write a Python program to remove the characters which have odd index values of a given string.
+
+
+def removeOdd(string):
+    count = 0
+    val = ''
+    for _ in range(len(string)-1):
+        if count % 2 == 0:
+            val += string[count]
+            count += 1
+        else:
+            count += 1
+    return val
+
+
+quest10 = input('return the even value from the string >> ')
+print()
+print(removeOdd(quest10))
